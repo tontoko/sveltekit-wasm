d02f281e656725e2d507b8c26058f74a5706a3e5
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2021-07-12 12:11:59 +0900

    markdown変換動作

--- src/routes/markdown.svelte
+++ src/routes/markdown.svelte
@@ -7,9 +7,11 @@
 	import init, { pulldown_cmark, greet } from '../../markdown_wasm/pkg/markdown_wasm';
 
 	let text = '';
+	let html = '';
 	const handleTextChange = async () => {
 		await init();
-		greet();
+		// greet();
+		html = pulldown_cmark(text);
 	};
 </script>
 
@@ -18,7 +20,8 @@
 </svelte:head>
 
 <div class="content">
-	<textarea on:change={handleTextChange} bind:value={text} />
+	<textarea on:keydown={handleTextChange} bind:value={text} />
+	{@html html}
 </div>
 
 <style>
@@ -26,5 +29,9 @@
 		width: 100%;
 		max-width: var(--column-width);
 		margin: var(--column-margin-top) auto 0 auto;
+	}
+	textarea {
+		width: 100%;
+		min-height: 100px;
 	}
 </style>
